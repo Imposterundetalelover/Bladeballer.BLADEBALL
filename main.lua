979d3812f144fc5e9a4a5faeb43c8efe85449264
`xeno <xlimo2002@gmail.com>
2023-09-12 19:18:29 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Window = Rayfield:CreateWindow({
-   Name = " Blade Ball || DX Hub",
+   Name = "Blade Ball || DX Hub",
    LoadingTitle = "DX Hub",
    LoadingSubtitle = "by xeno",
    ConfigurationSaving = {
