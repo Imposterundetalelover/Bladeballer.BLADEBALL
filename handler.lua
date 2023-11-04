eca4a00f9365f399064f9adb6bed2bc6971be0f1
Imposterundetalelover <112672496+Imposterundetalelover@users.noreply.github.com>
2023-11-04 23:49:43 +0800

    Update handler.lua

--- handler.lua
+++ handler.lua
@@ -3,7 +3,7 @@ local id = game.PlaceId
 local supported = { 13772394625 }
 
 if table.find(supported, id) then
-  loadstring(game:HttpGet('https://raw.githubusercontent.com/xenoszef/Fazed.BLADEBALL/main/main.lua'))()
+  loadstring(game:HttpGet('https://raw.githubusercontent.com/Imposterundetalelover/Bladeballer.BLADEBALL/main/main.lua'))()
 else
   game.Players.LocalPlayer:Kick("💨 Fazed | Invalid Game")
 end
